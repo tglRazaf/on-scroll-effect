f62ef33d48a0f1cce3a7743f57b957ce89025c08
stephano <stephanorazafindramena.com>
2022-08-23 22:18:47 +0300

    remove unecessary files for prod build

--- main.js
+++
@@ -1,8 +0,0 @@
-
-import AWA from './src/js/awa.js';
-
-AWA.init({
-  once: false,
-  rootMargin: `0px 0px 0px 0px`,
-  elementRatio: 1
-})
--- src/js/awa.js
+++ src/js/awa.js
@@ -1,34 +1,30 @@
+import "../css/awa.css";
 
-
-
-
-import '../css/awa.css'
-
-import {ScrollObserver} from './libs/scrollObserver.js'
-const elements = document.querySelectorAll('*')
+import { ScrollObserver } from "./libs/scrollObserver.js";
+const elements = document.querySelectorAll("*");
 
 export default class AWA {
-    static init = ({
-        once = options.once,
-        rootMargin = options.rootMargin, 
-        elementRatio = options.elementRatio,
-    }) => {
-        const observer = new ScrollObserver({once, rootMargin, elementRatio})
-        elements.forEach(element => {
-            const awaProperty = element.getAttribute('data-awa')
-            if(awaProperty){
-                element.classList.add('pre-awa-hidden')
-                observer.observe(element, awaProperty)
-            }
-        });
-    }
+  static init = ({
+    once = options.once,
+    rootMargin = options.rootMargin,
+    elementRatio = options.elementRatio,
+  }) => {
+    const observer = new ScrollObserver({ once, rootMargin, elementRatio });
+    elements.forEach((element) => {
+      const awaProperty = element.getAttribute("data-awa");
+      if (awaProperty) {
+        element.classList.add("pre-awa-hidden");
+        observer.observe(element, awaProperty);
+      }
+    });
+  };
 }
 
 /**
  * Default options
  */
 const options = {
-    once: true,
-    rootMargin: `${window.innerHeight}px 0px 0px 0px`, 
-    elementRatio: 1,
-}
+  once: true,
+  rootMargin: `${window.innerHeight}px 0px 0px 0px`,
+  elementRatio: 1,
+};
